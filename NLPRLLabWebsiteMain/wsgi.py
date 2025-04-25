957fe9cb4ccf6fd975bccc57b00d70002131e2f8
Anil Kumar Singh <aiklavya@gmail.com>
2025-04-25 17:09:51 +0530

    After forking the repository from the Umesh Krishna one, reproducting the coda environment and creating a PyCharm project from it. Currently working. The next attemp will be to modularize the apps and the whole project.

--- NLPRLLabWebsiteMain/wsgi.py
+++ NLPRLLabWebsiteMain/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for nlp_labb project.
+WSGI config for NLPRLLabWebsiteMain project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nlp_labb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NLPRLLabWebsiteMain.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -11,7 +11,7 @@ INSTALLED_APPS = [
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nlp_labb.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NLPRLLabWebsiteMain.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
